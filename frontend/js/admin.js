c6efcd38c32b5bcbb32ef6f7ed3b94588ef4de49
andreylson12 <Andreylsonvirtual@gmail.com>
2026-05-20 10:53:19 -0300

    Update admin.js

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,49 +1,119 @@
-async function cadastrarProduto(){
+let produtos = [];
 
-const nome = document.getElementById('nome').value;
-const preco = document.getElementById('preco').value;
-const categoria = document.getElementById('categoria').value;
-const imagem = document.getElementById('imagem').value;
+async function carregarProdutos(){
 
-await fetch('http://localhost:3000/produtos',{
-method:'POST',
-headers:{
-'Content-Type':'application/json'
-},
-body:JSON.stringify({
+const response = await fetch(
+'produtos.json'
+);
+
+produtos = await response.json();
+
+atualizarLista();
+
+}
+
+function cadastrarProduto(){
+
+const nome =
+document.getElementById('nome').value;
+
+const preco =
+Number(document.getElementById('preco').value);
+
+const categoria =
+document.getElementById('categoria').value;
+
+const imagem =
+document.getElementById('imagem').value;
+
+if(!nome || !preco || !imagem){
+
+alert('Preencha todos campos');
+
+return;
+
+}
+
+produtos.push({
 nome,
 preco,
 categoria,
 imagem
-})
 });
 
-alert('Produto cadastrado');
+atualizarLista();
 
-listarProdutos();
+limparCampos();
 
 }
 
-async function listarProdutos(){
+function atualizarLista(){
 
-const response = await fetch('http://localhost:3000/produtos');
-
-const produtos = await response.json();
-
-const lista = document.getElementById('listaProdutos');
+const lista =
+document.getElementById('lista');
 
 lista.innerHTML = '';
 
-produtos.forEach(produto => {
+produtos.forEach((produto,index)=>{
 
 lista.innerHTML += `
+
+<div class="itemAdmin">
+
+<h3>${produto.nome}</h3>
+
 <p>
-${produto.nome} - R$ ${produto.preco}
+💰 R$ ${produto.preco}
 </p>
+
+<p>
+📂 ${produto.categoria}
+</p>
+
+<p>
+🖼️ ${produto.imagem}
+</p>
+
+<button onclick="removerProduto(${index})"
+style="
+margin-top:10px;
+background:red;
+color:white;
+padding:10px;
+border:none;
+border-radius:10px;
+cursor:pointer;
+">
+Excluir
+</button>
+
+</div>
+
 `;
 
 });
 
+document.getElementById('jsonArea').value =
+JSON.stringify(produtos,null,2);
+
 }
 
-listarProdutos();
+function removerProduto(index){
+
+produtos.splice(index,1);
+
+atualizarLista();
+
+}
+
+function limparCampos(){
+
+document.getElementById('nome').value = '';
+
+document.getElementById('preco').value = '';
+
+document.getElementById('imagem').value = '';
+
+}
+
+carregarProdutos();
